fee22d203daa5164ae0180bfea72efe549ef5ac4
haiphamkd <91818517+haiphamkd@users.noreply.github.com>
2025-12-05 13:46:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Quan trọng: Giúp đường dẫn file hoạt động trên mọi hosting
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false,
